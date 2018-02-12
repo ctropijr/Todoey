df154b533c0ba068a1cd49860954543e60adad28
Christopher Tropiano <christophertropiano@Christophers-MacBook-Pro-2.local>
2018-02-12 17:39:40 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Christopher Tropiano on 2/12/18.
+//  Copyright © 2018 Christopher Tropiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
